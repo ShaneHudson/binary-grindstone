154fb2ceefcc7fdde7f1ed9f36c58dd334b558d0
Gideon MW Jones <gideon.jones@ntlworld.com>
2014-02-08 14:30:05 +0000

    nicer css

--- web/src/main.js
+++ web/src/main.js
@@ -3,9 +3,9 @@ var DEV = true;
 $(function() {
    $.getJSON('src/data.json', function (data) {
       if (DEV) { console.log(data); }
-      //fill_tagline(data);
+      fill_tagline(data);
       //fill_description(data);      
-      //fill_discography(data);
+      fill_discography(data);
    });
 });
 
