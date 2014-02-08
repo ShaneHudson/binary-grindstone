3d9e98a746aa7b9c6f4b02183b110742f4950a88
Gideon MW Jones <gideon.jones@ntlworld.com>
2014-02-08 13:28:43 +0000

    now with ajax

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ $(function() {
    
    console.log('dong stuff');
    
-   jQuery.getJSON('src/data.json', function loaded_data(data) {
+   $.getJSON('src/data.json', function loaded_data(data) {
       console.log(data);
       
       //fill_description(data);      
