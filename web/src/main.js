1d7c2d0b5995377aa2a5d06d86025a1820c3b44b
Gideon MW Jones <gideon.jones@ntlworld.com>
2014-02-08 13:23:53 +0000

    now with ajax

---
+++ web/src/main.js
@@ -0,0 +1,28 @@
+$(function() {
+   
+   jQuery.getJSON('data.json', function loaded_data(data) {
+      //fill_description(data);      
+      fill_discography(data);
+   });
+});
+
+
+
+function fill_discography(data) {
+   for (var i = 0; i < data.albums.length; i++) {
+      var tbody = $('<tbody />')
+      
+      for (var j = 0; j < data.albums[i].tracks.length; j++) {
+         tbody.append(
+            $('<tr />').append(
+               $('<td />').text(data.albums[i].tracks[j])
+         ))      
+      }
+      
+      $('<table />').append(
+         $('<thead />').append(
+            $('<tr />').append(
+               $('<th />').text('Track name')
+      )).append(tbody));  
+   }   
+}
